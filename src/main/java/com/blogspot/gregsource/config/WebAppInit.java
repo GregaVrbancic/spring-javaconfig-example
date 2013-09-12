ee835df128e406a894317b8159e216ca6375a3b6
GregaVrbancic <grega.vrbancic@gmail.com>
2013-09-12 17:18:47 +0200

    added character encoding filter

--- src/main/java/com/blogspot/gregsource/config/WebAppInit.java
+++ src/main/java/com/blogspot/gregsource/config/WebAppInit.java
@@ -3,8 +3,10 @@ package com.blogspot.gregsource.config;
 import com.blogspot.gregsource.config.WebConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -26,6 +28,11 @@ public class WebAppInit implements WebApplicationInitializer {
         ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
         servlet.addMapping("/");
         servlet.setLoadOnStartup(1);
-    }
 
+        //character encoding filter
+        FilterRegistration.Dynamic filterRegistration = servletContext.addFilter("characterEncodingFilter", new CharacterEncodingFilter());
+        filterRegistration.setInitParameter("encoding", "UTF-8");
+        filterRegistration.setInitParameter("forceEncoding", "true");
+        filterRegistration.addMappingForUrlPatterns(null, true, "/*");
+    }
 }
